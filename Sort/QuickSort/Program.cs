dc58c841a504c4b05fd6955293cf165d5cd801ad
smcilvenny <s.mcilvenny@students.clark.edu>
2019-12-09 03:14:00 -0800

    QUicksort done

---
+++ Sort/QuickSort/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    class Program
+    {
+
+        private static void QuickSort(int[] arr, int left, int right)
+        {
+            if(left < right)
+            {
+                int pivot = Partition(arr, left, right);
+                if(pivot > 1)
+                {
+                    QuickSort(arr, left, pivot - 1);
+                }
+                if (pivot + 1 < right)
+                {
+                    QuickSort(arr, pivot + 1, right);
+                }
+            }
+        }
+        private static int Partition(int[] arr, int left, int right)
+        {
+            int pivot = arr[left];
+            while (true)
+            {
+                while (arr[left] < pivot)
+                {
+                    left++;
+                }
+                while (arr[right] > pivot)
+                {
+                    right--;
+                }
+                if (left < right)
+                {
+                    int temp = arr[right];
+                    arr[right] = arr[left];
+                    arr[left] = temp;
+                }
+                else
+                {
+                    return right;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 10, 13, 16, 133, 99, 43, 0, 4, 17, 87, 21, 56, -1 };
+            Console.WriteLine("Original Array: \n");
+            for(int i=0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + ", ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            QuickSort(arr, 0, arr.Length - 1);
+            Console.WriteLine("After QuickSort: \n");
+            foreach (int number in arr)
+            {
+                Console.Write(number + ", ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
